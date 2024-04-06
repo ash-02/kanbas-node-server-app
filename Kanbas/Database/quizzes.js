63b2a75a0eedd304455601be7d76a6af3b479744
Ashwin Sathiyanarayanan <66624673+ash-02@users.noreply.github.com>
2024-04-06 18:45:40 -0400

    Initial Quiz Changes

--- Kanbas/Database/quizzes.js
+++ Kanbas/Database/quizzes.js
@@ -29,5 +29,67 @@ export default [
                 "points": 2
             }
         ]
+    },
+    {
+        "_id": "Q102",
+        "title": "Quiz 2",
+        "course": "RS101",
+        "points": 10,
+        "due": "2024-04-10T01:50:25Z",
+        "available": "2024-04-03T01:50:25Z",
+        "publishStatus": 1,
+        "numQuestions": 3,
+        "questions": [
+            {
+                "questionType": "multipleChoice",
+                "question": "What is the capital of Germany?",
+                "choices": ["Paris", "London", "Berlin", "Madrid"],
+                "correctAnswer": "Berlin",
+                "points": 2
+            },
+            {
+                "questionType": "trueFalse",
+                "question": "Is the Sun a planet?",
+                "correctAnswer": false,
+                "points": 2
+            },
+            {
+                "questionType": "fillInBlank",
+                "question": "The capital of Spain is __________.",
+                "correctAnswer": "Madrid",
+                "points": 2
+            }
+        ]
+    },
+    {
+        "_id": "Q103",
+        "title": "Quiz 3",
+        "course": "RS101",
+        "points": 10,
+        "due": "2024-10-02",
+        "available": "2024-04-08T01:50:25Z",
+        "publishStatus": 1,
+        "numQuestions": 3,
+        "questions": [
+            {
+                "questionType": "multipleChoice",
+                "question": "What is the capital of the United Kingdom?",
+                "choices": ["Paris", "London", "Berlin", "Madrid"],
+                "correctAnswer": "London",
+                "points": 2
+            },
+            {
+                "questionType": "trueFalse",
+                "question": "Is the Moon a star?",
+                "correctAnswer": false,
+                "points": 2
+            },
+            {
+                "questionType": "fillInBlank",
+                "question": "The capital of France is __________.",
+                "correctAnswer": "Paris",
+                "points": 2
+            }
+        ]
     }
 ]
