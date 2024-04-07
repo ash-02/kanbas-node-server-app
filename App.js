92c67c324b31bfeb2a727d3face7456707681087
Ashwin Sathiyanarayanan <66624673+ash-02@users.noreply.github.com>
2024-04-07 11:14:55 -0400

    Initial progress on A6

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from 'cors'
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors())
 app.use(express.json());
@@ -13,4 +16,5 @@ ModuleRoutes(app)
 CourseRoutes(app)
 Lab5(app)
 Hello(app)
+UserRoutes(app);
 app.listen(process.env.PORT || 4000)
