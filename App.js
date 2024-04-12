440741a363e21cde319bbba06f7f43aa8865b4b7
Ashwin Sathiyanarayanan <66624673+ash-02@users.noreply.github.com>
2024-04-12 11:22:43 -0400

    Session changes

--- App.js
+++ App.js
@@ -11,22 +11,21 @@ import "dotenv/config";
 import session from "express-session";
 
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
     credentials: true,
     origin: process.env.FRONTEND_URL,
-  })
+  }),
 );
 
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-};
+// const sessionOptions = {
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+// };
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -34,7 +33,7 @@ if (process.env.NODE_ENV !== "development") {
     secure: true,
   };
 }
-app.use(session(sessionOptions));
+// app.use(session(sessionOptions));
 
 app.use(express.json());
 AssignmentRoutes(app);
